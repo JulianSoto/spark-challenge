6c85746d95186080cb6e00936bac0f048b7d512f
juliansoto <juliansotoponce@gmail.com>
2024-12-04 11:39:36 -0800

    chore: install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.status(204).end();
+});
+
+app.listen(PORT, () => console.log('listening on port', PORT));
